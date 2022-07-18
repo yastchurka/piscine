3d7372a6bcd492e77e1cae6b03c3bebfacc2300d
yastchurka <yastchurka@mailbox.org>
2022-07-18 11:02:30 +0200

    abc

---
+++ C01/ex00/ft_ft.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int ft_ft(int *nbr)
+{
+
+    *nbr = 42;
+    return (*nbr);
+}
+
+/*
+int main()
+{
+    int nbr;
+
+    nbr = 5;
+    printf("%d\n", nbr);
+    ft_ft(&nbr);
+    printf("%d\n", nbr);
+}
+*/
